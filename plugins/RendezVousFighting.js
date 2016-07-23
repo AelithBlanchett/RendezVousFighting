44895e028ddb8f781ded8e256e389dea6cc43b90
Aelith Blanchett <tonyteur@yahoo.fr>
2016-07-23 15:03:03 -0400

    command to try stage bug

--- plugins/RendezVousFighting.js
+++ plugins/RendezVousFighting.js
@@ -34,6 +34,14 @@ module.exports = function (parent, chanName) {
     var cmdHandler = {};
     channel = chanName;
 
+    cmdHandler.stage = function (args, data) {
+        var stages = [];
+        for(var i = 0; i < 100; i++){
+            stages.push(arena().pickStage());
+        }
+        fChatLibInstance.sendMessage(JSON.stringify(stages), channel);
+    };
+
     cmdHandler.stats = function (args, data) {
         statsGetter(args, data, data.character);
     };
